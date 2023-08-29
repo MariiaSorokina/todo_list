be99f09cc8d2b3266314c6b88cde18b48e2a887a
msorokina <m.sorokina0702@gmail.com>
2023-08-29 14:16:10 +0400

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('not_started', 'Not started'),
+        ('in_progress', 'In progress'),
+        ('urgent', 'Urgent to complete'),
+        ('completed', 'Completed'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='not_started')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['status']
